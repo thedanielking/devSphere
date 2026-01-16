b09e065792463ae2a0b0897423407cb59d8a0bc7
Daniel King <dking9486@gmail.com>
2026-01-17 00:47:36 +0100

    logout guard

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -5,41 +5,41 @@ import useLogout from "../features/authentication/useLogout";
 
 function SideNavBar({display}) {
     const logout = useLogout();
-    
+
     function handleLogout(){
         logout();
     }
 
     return (
         <ul className={`px-4 py-6 space-y-12 text-xl backdrop-blur-lg shadow-md
-        fixed left-0 h-screen w-64 z-50
+        fixed left-0 h-dvh w-64 z-50
         transform transition-transform duration-300 ease-in-out
         ${display ? 'translate-x-0' : '-translate-x-full'}
         lg:static lg:translate-x-0 lg:z-auto lg:block lg:w-64 lg:transition-none
   `}>
             
-            <Link to="/" className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded">
-                <GoHome className="text-primary" />
+            <Link to="/" className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded lg:text-xl">
+                <GoHome className="text-primary text-xl lg:text-3xl" />
                 <span>Home</span>                
             </Link>
             
-            <Link className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded">
-                <GoPerson className="text-primary" />
+            <Link className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded lg:text-xl">
+                <GoPerson className="text-primary text-xl lg:text-3xl" />
                 <span>Profile</span>
             </Link>
 
-            <Link className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded">
-                <GoBookmark className="text-primary" />
+            <Link className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded lg:text-xl">
+                <GoBookmark className="text-primary text-xl lg:text-3xl" />
                 <span>Library</span>
             </Link>
 
-            <Link className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded">
-                <CiSettings className="text-primary" />
+            <Link className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded lg:text-xl">
+                <CiSettings className="text-primary text-xl lg:text-3xl" />
                 <span>Settings</span>
             </Link>
 
-            <li className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded" onClick={()=>handleLogout()}>
-                <CiLogout className="text-primary" />
+            <li className="px-3 py-4 flex items-center gap-2 hover:bg-white/30 cursor-pointer rounded lg:text-xl" onClick={()=>handleLogout()}>
+                <CiLogout className="text-primary text-xl lg:text-3xl" />
                 <span>Logout</span>
             </li>
         </ul>
--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,9 +1,12 @@
 import toast from "react-hot-toast";
 import { logout as logoutApi } from "../../services/apiAuth";
+import { useAuth } from "../../context/AuthContext";
 
 function useLogout(){
+    const {user} = useAuth();
     const logout = async () => {
         try{
+            if(!user) throw new Error("No user is currently logged in.");
             await logoutApi();
             toast.success("Logged out successfully");
         }
