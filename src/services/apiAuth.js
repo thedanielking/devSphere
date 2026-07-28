ce7b45b70bf12b2ad40f679bb7a6f53d25930f8e
Daniel King <dking9486@gmail.com>
2026-07-28 21:55:53 +0100

    google redirect

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -31,7 +31,8 @@ export async function googleSignIn() {
         provider: 'google',
         options: {
           // Redirects back to your local React site after a successful login
-          redirectTo: 'http://localhost:5173', 
+            //   redirectTo: 'http://localhost:5173', 
+            redirectTo: 'https://devsphereng.vercel.app/', 
         },
     });
 
